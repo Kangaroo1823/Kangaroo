47585b7a937a9362050517e048092df871a00cd1
pml76 <peter.lennartz@gmail.com>
2025-01-31 16:13:39 +0100

    more (but not all) tests are working again

--- test/movement_generator_tests.cpp
+++ test/movement_generator_tests.cpp
@@ -26,6 +26,12 @@ namespace Kangaroo::Movement_Generator_Tests {
         ASSERT_EQ(gen.get_pin_mask_D(), value) << str;
     }
 
+    Status_Callback_Template(Compare_Pin_Mask_D2, Chess_Board * board, const Bitboard value, const std::string& str) {
+        Move_Generator::Move_Generator<status, CallbackType> gen(board);
+        print_bitboard(gen.get_pin_mask_D());
+        ASSERT_EQ(gen.get_pin_mask_D(), value) << str;
+    }
+
     TEST(Movement_Generator_Test, test_pin_masks_generator) {
         using enum Color;
         using enum Pin_Masks_Suitable_For;
@@ -46,7 +52,7 @@ namespace Kangaroo::Movement_Generator_Tests {
         execute_status_callback_template<Compare_Pin_Mask_HV, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x00, "3rd test not true");
 
-        s = board.reset_board("k7/8/8/8/p7/8/R7/8 w - - 0 1 ");
+        s = board.reset_board("k7/8/8/8/p7/8/R7/8 b - - 0 1 ");
         execute_status_callback_template<Compare_Pin_Mask_HV, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x1010101010100, "4th test not true");
 
@@ -54,7 +60,7 @@ namespace Kangaroo::Movement_Generator_Tests {
         execute_status_callback_template<Compare_Pin_Mask_HV, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x1010101010100, "5th test not true");
 
-        s = board.reset_board("k7/8/8/8/p7/8/Q7/8 w - - 0 1 ");
+        s = board.reset_board("k7/8/8/8/p7/8/Q7/8 b - - 0 1 ");
         execute_status_callback_template<Compare_Pin_Mask_HV, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x1010101010100, "6th test not true");
 
@@ -62,7 +68,7 @@ namespace Kangaroo::Movement_Generator_Tests {
         execute_status_callback_template<Compare_Pin_Mask_HV, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x1010101010100, "7th test not true");
 
-        s = board.reset_board("8/Q7/8/p7/8/8/8/k7 w - - 0 1 ");
+        s = board.reset_board("8/Q7/8/p7/8/8/8/k7 b - - 0 1 ");
         execute_status_callback_template<Compare_Pin_Mask_HV, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x1010101010100, "8th test not true");
 
@@ -70,7 +76,7 @@ namespace Kangaroo::Movement_Generator_Tests {
         execute_status_callback_template<Compare_Pin_Mask_HV, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x1010101010100, "9th test not true");
 
-        s = board.reset_board("8/R7/8/p7/8/8/8/k7 w - - 0 1 ");
+        s = board.reset_board("8/R7/8/p7/8/8/8/k7 b - - 0 1 ");
         execute_status_callback_template<Compare_Pin_Mask_HV, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x1010101010100, "10th test not true");
 
@@ -78,15 +84,15 @@ namespace Kangaroo::Movement_Generator_Tests {
         execute_status_callback_template<Compare_Pin_Mask_HV, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x00, "11th test not true");
 
-        s = board.reset_board("8/R7/8/p7/p7/8/8/k7 w - - 0 1 ");
+        s = board.reset_board("8/R7/8/p7/p7/8/8/k7 b - - 0 1 ");
         execute_status_callback_template<Compare_Pin_Mask_HV, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x00, "12th test not true");
 
-        s = board.reset_board("8/8/R1pp2k1/8/8/8/8/8 w - - 0 1 ");
+        s = board.reset_board("8/8/R1pp2k1/8/8/8/8/8 b - - 0 1 ");
         execute_status_callback_template<Compare_Pin_Mask_HV, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x00, "13th test not true");
 
-        s = board.reset_board("8/8/R2p2k1/8/8/8/8/8 w - - 0 1 ");
+        s = board.reset_board("8/8/R2p2k1/8/8/8/8/8 b - - 0 1 ");
         execute_status_callback_template<Compare_Pin_Mask_HV, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x3f0000000000, "14th test not true");
 
@@ -106,7 +112,7 @@ namespace Kangaroo::Movement_Generator_Tests {
         execute_status_callback_template<Compare_Pin_Mask_HV, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x0, "18th test not true");
 
-        s = board.reset_board("8/8/k2p2R1/8/8/8/8/8 w - - 0 1 ");
+        s = board.reset_board("8/8/k2p2R1/8/8/8/8/8 b - - 0 1 ");
         execute_status_callback_template<Compare_Pin_Mask_HV, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x7e0000000000, "19th test not true");
 
@@ -122,7 +128,7 @@ namespace Kangaroo::Movement_Generator_Tests {
         execute_status_callback_template<Compare_Pin_Mask_HV, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x0, "22th test not true");
 
-        s = board.reset_board("8/8/k2p2Q1/8/8/8/8/8 w - - 0 1 ");
+        s = board.reset_board("8/8/k2p2Q1/8/8/8/8/8 b - - 0 1 ");
         execute_status_callback_template<Compare_Pin_Mask_HV, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x7e0000000000, "23th test not true");
 
@@ -136,7 +142,7 @@ namespace Kangaroo::Movement_Generator_Tests {
         execute_status_callback_template<Compare_Pin_Mask_D, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x0, "25th test not true");
 
-        s = board.reset_board("8/1k6/8/3p4/8/8/6B/8 w - - 0 1 ");
+        s = board.reset_board("8/1k6/8/3p4/8/8/6B/8 b - - 0 1 ");
         execute_status_callback_template<Compare_Pin_Mask_D, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x40810204000, "26th test not true");
 
@@ -148,11 +154,11 @@ namespace Kangaroo::Movement_Generator_Tests {
         execute_status_callback_template<Compare_Pin_Mask_D, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x40810204000, "28th test not true");
 
-        s = board.reset_board("8/1k6/8/3p4/4p3/8/6Q/8 w - - 0 1 ");
+        s = board.reset_board("8/1k6/8/3p4/4p3/8/6Q/8 b - - 0 1 ");
         execute_status_callback_template<Compare_Pin_Mask_D, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x0, "29th test not true");
 
-        s = board.reset_board("8/1k6/8/3p4/8/8/6Q/8 w - - 0 1 ");
+        s = board.reset_board("8/1k6/8/3p4/8/8/6Q/8 b - - 0 1 ");
         execute_status_callback_template<Compare_Pin_Mask_D, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x40810204000, "30th test not true");
 
@@ -164,13 +170,14 @@ namespace Kangaroo::Movement_Generator_Tests {
         execute_status_callback_template<Compare_Pin_Mask_D, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x40810204000, "32th test not true");
 
-        s = board.reset_board("8/6k1/8/4p3/3p4/8/1B6/8 w - - 0 1 ");
+        s = board.reset_board("8/6k1/8/4p3/3p4/8/1B6/8 b - - 0 1 ");
         execute_status_callback_template<Compare_Pin_Mask_D, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x0, "33th test not true");
 
-        s = board.reset_board("8/6k1/8/8/3p4/8/1B6/8 w - - 0 1 ");
+        s = board.reset_board("8/6k1/8/8/3p4/8/1B6/8 b - - 0 1 ");
+        print_chess_board(board);
         execute_status_callback_template<Compare_Pin_Mask_D, Dummy, Chess_Board *, const Bitboard>(
-            *s, &board, 0x40810204000, "34th test not true");
+            *s, &board, 0x201008040200, "34th test not true");
 
         s = board.reset_board("8/6K1/8/4P3/3P4/8/1b6/8 w - - 0 1 ");
         execute_status_callback_template<Compare_Pin_Mask_D, Dummy, Chess_Board *, const Bitboard>(
@@ -180,11 +187,11 @@ namespace Kangaroo::Movement_Generator_Tests {
         execute_status_callback_template<Compare_Pin_Mask_D, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x201008040200, "36th test not true");
 
-        s = board.reset_board("8/6k1/8/4p3/3p4/8/1Q6/8 w - - 0 1 ");
+        s = board.reset_board("8/6k1/8/4p3/3p4/8/1Q6/8 b - - 0 1 ");
         execute_status_callback_template<Compare_Pin_Mask_D, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x0, "37th test not true");
 
-        s = board.reset_board("8/6k1/8/8/3p4/8/1Q6/8 w - - 0 1 ");
+        s = board.reset_board("8/6k1/8/8/3p4/8/1Q6/8 b - - 0 1 ");
         execute_status_callback_template<Compare_Pin_Mask_D, Dummy, Chess_Board *, const Bitboard>(
             *s, &board, 0x201008040200, "38th test not true");
 
@@ -1326,7 +1333,7 @@ A  B  C  D  E  F  G  H
         Chess_Board board{};
         const auto status = board.reset_board("8/8/8/8/3Pp3/8/PPP1PPPP/8 b kqKQ d3 0 1");
 
-        print_chess_board(board);
+
 
         [[maybe_unused]] std::array<Chess_Board, 2> new_boards{
             /*
